dd84b08315704aca4ccef5c0efb116325446cc9f
nelson131 <85147519+nelson131@users.noreply.github.com>
2022-12-31 18:54:39 +0300

    for end

--- src/main/java/me/nelson131/nes/ChunkListener.java
+++ src/main/java/me/nelson131/nes/ChunkListener.java
@@ -3,6 +3,7 @@ package me.nelson131.nes;
 import io.papermc.paper.event.packet.PlayerChunkLoadEvent;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
@@ -12,20 +13,24 @@ import static me.nelson131.nes.NoElytraSpawn.plugin;
 
 public class ChunkListener implements Listener {
     @EventHandler
-    public void onChunk(PlayerChunkLoadEvent event){
+    public void onChunk(PlayerChunkLoadEvent event) {
 
         Boolean elytraspawn = plugin.getConfig().getBoolean("disable-elytra-spawn");
         Chunk chunk = event.getChunk();
 
-        if(elytraspawn == true){
-            for(Entity entity : chunk.getEntities()){
-                if(entity instanceof ItemFrame){
-                    ItemFrame frame = (ItemFrame) entity;
-                    if(frame.getItem().getType() == Material.ELYTRA){
-                        frame.remove();
+        if (elytraspawn == true) {
+                if (chunk.getWorld().getEnvironment().equals(World.Environment.THE_END)) {
+                    for (Entity entity : chunk.getEntities()) {
+                        if (entity instanceof ItemFrame) {
+                            ItemFrame frame = (ItemFrame) entity;
+                            if (frame.getItem().getType() == Material.ELYTRA) {
+                                frame.remove();
+                            }
+                        }
                     }
                 }
             }
         }
     }
-}
+
+
