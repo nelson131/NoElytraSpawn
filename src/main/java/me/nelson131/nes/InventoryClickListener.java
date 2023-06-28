b2b8e164b4265333f2fcb35212809f33e7c40b4e
nelson131 <85147519+nelson131@users.noreply.github.com>
2023-06-28 20:34:23 +0300

    fix

--- src/main/java/me/nelson131/nes/InventoryClickListener.java
+++ src/main/java/me/nelson131/nes/InventoryClickListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import static me.nelson131.nes.NoElytraSpawn.plugin;
@@ -12,10 +13,14 @@ public class InventoryClickListener implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent event){
         ItemStack itemStack = event.getCurrentItem();
+        Inventory inventory = event.getClickedInventory();
         Boolean click = plugin.getConfig().getBoolean("disable-click-inventory");
         if(click == true){
-            if(itemStack.getType() == Material.ELYTRA){
-                itemStack.setAmount(0);
+            if(inventory.contains(Material.ELYTRA)){
+                if(itemStack.getType() == Material.ELYTRA){
+                    itemStack.setAmount(0);
+
+                }
             }
         }
     }
